f3e61956ed4d9e99b9ad606ab51a483dfd14f970
Matthias Urlichs <matthias@urlichs.de>
2017-06-22 10:48:13 +0200

    doc modbus gw

--- devrun/typ/gateway/modbus.py
+++ devrun/typ/gateway/modbus.py
@@ -13,6 +13,11 @@ from __future__ import absolute_import, print_function, division, unicode_litera
 ## Thus, please do not remove the next line, or insert any blank lines.
 ##BP
 
+# This code implements a Modbus gateway.
+# Its main use is to multiplex multiple clients to one connection,
+# in order to support dumb Modbus/TCP-enabled devices which cannot
+# handle more than one connection.
+
 import asyncio
 import sys
 from devrun.support.modbus import ModbusException
@@ -81,94 +86,25 @@ class RemoteSlaveContext(RSC):
     A forwarding modbus slave
     '''
 
-#    def __init__(self, client):
-#        ''' Initializes the datastores
-#
-#        :param client: The client to retrieve values with
-#        '''
-#        self._client = client
-#        self.__build_mapping()
-#
-#    def reset(self):
-#        ''' Resets all the datastores to their default values '''
-#        raise NotImplementedException()
-#
     def validate(self, fx, address, count=1):
-        ''' Validates the request to make sure it is in range
-
-        :param fx: The function we are working with
-        :param address: The starting address
-        :param count: The number of values to test
-        :returns: True if the request in within range, False otherwise
-        '''
-        logger.debug("validate[%d] %d:%d" % (fx, address, count))
+        ''' Validation is disabled here '''
         return True
-#
+
     def getValues(self, fx, address, count=1):
-        ''' Validates the request to make sure it is in range
+        ''' Get values from the remote server
 
         :param fx: The function we are working with
         :param address: The starting address
         :param count: The number of values to retrieve
-        :returns: The requested values from a:a+c
+        :returns: A Deferred with the requested values from a:a+c
         '''
-        # TODO deal with deferreds
-        #import pdb;pdb.set_trace()
         logger.debug("get values[%d] %d:%d" % (fx, address, count))
         result = self.__get_callbacks[self.decode(fx)](address, count)
         result.addCallback(lambda r: self.__extract_result(self.decode(fx), r))
-        #result = await result
         return result
 
-#    def setValues(self, fx, address, values):
-#        ''' Sets the datastore with the supplied values
-#
-#        :param fx: The function we are working with
-#        :param address: The starting address
-#        :param values: The new values to be set
-#        '''
-#        # TODO deal with deferreds
-#        logger.debug("set values[%d] %d:%d" % (fx, address, len(values)))
-#        return self.__set_callbacks[self.decode(fx)](address, values)
-#
-#    def __str__(self):
-#        ''' Returns a string representation of the context
-#
-#        :returns: A string representation of the context
-#        '''
-#        return "Remote Slave Context(%s)" % self._client
-#
-#    def __build_mapping(self):
-#        '''
-#        A quick helper method to build the function
-#        code mapper.
-#        '''
-#        self.__get_callbacks = {
-#            'd': lambda a, c: self._client.read_discrete_inputs(a, c),
-#            'c': lambda a, c: self._client.read_coils(a, c),
-#            'h': lambda a, c: self._client.read_holding_registers(a, c),
-#            'i': lambda a, c: self._client.read_input_registers(a, c),
-#        }
-#        self.__set_callbacks = {
-#            'd': lambda a, v: self._client.write_coils(a, v),
-#            'c': lambda a, v: self._client.write_coils(a, v),
-#            'h': lambda a, v: self._client.write_registers(a, v),
-#            'i': lambda a, v: self._client.write_registers(a, v),
-#        }
-#
-#    def __extract_result(self, fx, result):
-#        ''' A helper method to extract the values out of
-#        a response.  TODO make this consistent (values?)
-#        '''
-#        if result.function_code < 0x80:
-#            if fx in ['d', 'c']: return result.bits
-#            if fx in ['h', 'i']: return result.registers
-#        else: return result
-
 class TcpClient(ModbusClientProtocol):
-    def execute(self,request):
-        #import pdb;pdb.set_trace()
-        return super().execute(request)
+    pass
 
 class Device(BaseDevice):
     """ModBus gateway"""
